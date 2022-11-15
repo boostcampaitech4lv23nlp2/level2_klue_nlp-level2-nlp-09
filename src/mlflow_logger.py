5ed779b8990ea1d8322352e86565cc9db624bed7
kyc3492 <kyc3492@gmail.com>
2022-11-15 06:10:30 +0000

    style: fix style using styling functions of template

---
+++ src/mlflow_logger.py
@@ -0,0 +1,14 @@
+import os
+
+import mlflow.pytorch
+
+from mlflow_config import experiment_name, tracking_uri
+
+
+def set_mlflow_logger():
+    mlflow.set_tracking_uri(tracking_uri)
+    mlflow.set_experiment(experiment_name)
+    mlflow.pytorch.autolog(
+        log_every_n_step=1,
+    )
+    return
