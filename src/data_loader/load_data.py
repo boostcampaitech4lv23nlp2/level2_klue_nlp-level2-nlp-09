03111ea5cdf294d18bbdfe279eee0bc20c65a9d2
jinmyeongAN <jinmyeong.an@gmail.com>
2022-11-17 17:53:24 +0000

    feat    : make tokenized_dataset move into REDataset class * tokenized_dataset를 REDataset 클래스 안에서 관리 * 따라서 raw_dataset를 한 번에 preprocessing이 완료된 dataset으로 변환

--- src/data_loader/load_data.py
+++ src/data_loader/load_data.py
@@ -5,6 +5,40 @@ import pandas as pd
 import torch
 
 
+class REDataset(torch.utils.data.Dataset):
+    """Dataset 구성을 위한 class."""
+
+    def __init__(self, dataset, tokenizer, labels):
+        self.labels = labels
+        self.pair_dataset = self.tokenized_dataset(dataset, tokenizer)
+
+    def __getitem__(self, idx):
+        item = {key: val[idx].clone().detach() for key, val in self.pair_dataset.items()}
+        item["labels"] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+    def tokenized_dataset(self, dataset, tokenizer):
+        """tokenizer에 따라 sentence를 tokenizing 합니다."""
+        concat_entity = []
+        for e01, e02 in zip(dataset["subject_entity"], dataset["object_entity"]):
+            temp = ""
+            temp = e01 + "[SEP]" + e02
+            concat_entity.append(temp)
+        tokenized_sentences = tokenizer(
+            concat_entity,
+            list(dataset["sentence"]),
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            max_length=256,
+            add_special_tokens=True,
+        )
+        return tokenized_sentences
+
+
 def preprocessing_dataset(dataset):
     """처음 불러온 csv 파일을 원하는 형태의 DataFrame으로 변경 시켜줍니다."""
     subject_entity = []
