b897583ad0a3cb721c337f9aee68bda5957f4762
kyc3492 <kyc3492@gmail.com>
2022-11-18 16:28:00 +0000

    feat: port complete save_model_remote function to remade project

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,10 @@
 from .get_train_valid_split import get_train_valid_split
 from .set_seed import set_seed
-from .util import DataTrainingArguments, ModelArguments, get_training_args, label_to_num, set_mlflow_logger
+from .util import (
+    DataTrainingArguments,
+    ModelArguments,
+    get_training_args,
+    label_to_num,
+    save_model_remote,
+    set_mlflow_logger,
+)
