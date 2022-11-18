cf34fa175d33b2fb625fda7468d7464884dc7742
jinmyeongAN <jinmyeong.an@gmail.com>
2022-11-18 02:37:53 +0000

    fix     : seperate the constant value from train.py * train.py 코드 내부의 args 값을 외부에서 관리할 수 있도록 함 * 모델, 데이터, trainer의 args 변경시 train.py 코드를 건들이지 않게하기 위함 * 재사용성 고려

--- src/train.py
+++ src/train.py
@@ -21,27 +21,34 @@ from transformers import (
 
 from data_loader.load_data import REDataset, load_data
 from model.metric import compute_metrics
-from utils.util import DataTrainingArguments, ModelArguments, label_to_num, set_mlflow_logger
+from utils.util import DataTrainingArguments, ModelArguments, get_training_args, label_to_num, set_mlflow_logger
 
 
 def train():
     # Using HfArgumentParser we can turn this class into argparse arguments to be able to specify them on the command line.
-    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
-    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    # parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+    # model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments))
+    model_args, data_args = parser.parse_args_into_dataclasses()
+    training_args = get_training_args()
 
     # load dataset
     train_raw_dataset = load_data(data_args.train_file_path)
-    # dev_dataset = load_data("../dataset/train/dev.csv") # validation용 데이터는 따로 만드셔야 합니다.
+    # dev_raw_dataset = load_data(data_args.validation_file_path) # validation용 데이터는 따로 만드셔야 합니다.
 
+    # label
     train_label = label_to_num(train_raw_dataset["label"].values)
-    # dev_label = label_to_num(dev_dataset['label'].values)
+    # dev_label = label_to_num(dev_raw_dataset["label"].values)
 
     # setting model hyperparameter
-    model_config = AutoConfig.from_pretrained(model_args.model_name_or_path, num_labels=30)
+    num_labels = len(set(train_label))
+    model_config = AutoConfig.from_pretrained(model_args.model_name_or_path, num_labels=num_labels)
 
     # load model and tokenizer
     tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
 
+    # model
     model = AutoModelForSequenceClassification.from_pretrained(model_args.model_name_or_path, config=model_config)
     print(model.config)
 
@@ -51,32 +58,11 @@ def train():
 
     model.to(device)
 
-    # Preprocessing the raw_datasets
+    # Preprocessing the raw_datasets.
     # make dataset for pytorch.
     train_dataset = REDataset(train_raw_dataset, tokenizer, train_label)
-    # RE_dev_dataset = RE_Dataset(tokenized_dev, dev_label)
+    # dev_dataset = REDataset(dev_raw_dataset, tokenizer, dev_label)
 
-    # 사용한 option 외에도 다양한 option들이 있습니다.
-    # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
-    training_args = TrainingArguments(
-        output_dir="./results",  # output directory
-        save_total_limit=5,  # number of total save model.
-        save_steps=500,  # model saving step.
-        num_train_epochs=1,  # total number of training epochs
-        learning_rate=5e-5,  # learning_rate
-        per_device_train_batch_size=32,  # batch size per device during training
-        per_device_eval_batch_size=32,  # batch size for evaluation
-        warmup_steps=500,  # number of warmup steps for learning rate scheduler
-        weight_decay=0.01,  # strength of weight decay
-        logging_dir="./logs",  # directory for storing logs
-        logging_steps=100,  # log saving step.
-        evaluation_strategy="steps",  # evaluation strategy to adopt during training
-        # `no`: No evaluation during training.
-        # `steps`: Evaluate every `eval_steps`.
-        # `epoch`: Evaluate every end of epoch.
-        eval_steps=500,  # evaluation step.
-        load_best_model_at_end=True,
-    )
     trainer = Trainer(
         model=model,  # the instantiated 🤗 Transformers model to be trained
         args=training_args,  # training arguments, defined above
