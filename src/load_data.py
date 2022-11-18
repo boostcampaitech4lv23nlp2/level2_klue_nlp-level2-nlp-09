12ec0f7adb6546e46e5e4535a427a949e2e5836b
wbbin0718 <wbbin0718@gmail.com>
2022-11-18 06:16:44 +0000

    fix: subject or object , split indexing

--- src/load_data.py
+++ src/load_data.py
@@ -4,6 +4,8 @@ import pickle as pickle
 import pandas as pd
 import torch
 
+from utils.representation import entity_representation
+
 
 class RE_Dataset(torch.utils.data.Dataset):
     """Dataset 구성을 위한 class."""
@@ -23,24 +25,7 @@ class RE_Dataset(torch.utils.data.Dataset):
 
 def preprocessing_dataset(dataset):
     """처음 불러온 csv 파일을 원하는 형태의 DataFrame으로 변경 시켜줍니다."""
-    subject_entity = []
-    object_entity = []
-    for i, j in zip(dataset["subject_entity"], dataset["object_entity"]):
-        i = i[1:-1].split(",")[0].split(":")[1]
-        j = j[1:-1].split(",")[0].split(":")[1]
-
-        subject_entity.append(i)
-        object_entity.append(j)
-    out_dataset = pd.DataFrame(
-        {
-            "id": dataset["id"],
-            "sentence": dataset["sentence"],
-            "subject_entity": subject_entity,
-            "object_entity": object_entity,
-            "label": dataset["label"],
-        }
-    )
-    return out_dataset
+    return dataset
 
 
 def load_data(dataset_dir):
@@ -54,13 +39,12 @@ def load_data(dataset_dir):
 def tokenized_dataset(dataset, tokenizer):
     """tokenizer에 따라 sentence를 tokenizing 합니다."""
     concat_entity = []
-    for e01, e02 in zip(dataset["subject_entity"], dataset["object_entity"]):
-        temp = ""
-        temp = e01 + "[SEP]" + e02
+    for e01, e02, sentence in zip(dataset["subject_entity"], dataset["object_entity"], dataset["sentence"]):
+
+        temp = entity_representation(e01, e02, sentence, method=None)
         concat_entity.append(temp)
     tokenized_sentences = tokenizer(
         concat_entity,
-        list(dataset["sentence"]),
         return_tensors="pt",
         padding=True,
         truncation=True,
--- src/utils/representation.py
+++ src/utils/representation.py
@@ -21,16 +21,19 @@ def entity_representation(subject: str, object: str, sentence: str, method: str 
 
     # subject object extraction
 
-    subject_entity = subject[:-1].split(",")[3].split(":")[1]
-    object_entity = object[:-1].split(",")[3].split(":")[1]
+    subject_entity = subject[:-1].split(",")[-1].split(":")[1]
+    object_entity = object[:-1].split(",")[-1].split(":")[1]
 
-    sub_start_idx = int(subject.split(",")[1].split(":")[1])
-    sub_end_idx = int(subject.split(",")[2].split(":")[1])
-    subject = subject.split(",")[0].split(":")[1]
+    subject_length = len(subject.split(","))
+    object_length = len(object.split(","))
 
-    obj_start_idx = int(object.split(",")[1].split(":")[1])
-    obj_end_idx = int(object.split(",")[2].split(":")[1])
-    object = object.split(",")[0].split(":")[1]
+    sub_start_idx = int(subject.split(",", subject_length - 3)[subject_length - 3].split(",")[0].split(":")[1])
+    sub_end_idx = int(subject.split(",", subject_length - 3)[subject_length - 3].split(",")[1].split(":")[1])
+    subject = "".join(subject.split(",", subject_length - 3)[:subject_length]).split(":")[1]
+
+    obj_start_idx = int(object.split(",", object_length - 3)[object_length - 3].split(",")[0].split(":")[1])
+    obj_end_idx = int(object.split(",", object_length - 3)[object_length - 3].split(",")[1].split(":")[1])
+    object = "".join(object.split(",", object_length - 3)[:object_length]).split(":")[1]
 
     subject_entity = subject_entity.replace("'", "").strip()
     object_entity = object_entity.replace("'", "").strip()
@@ -39,7 +42,7 @@ def entity_representation(subject: str, object: str, sentence: str, method: str 
 
     # baseline code
     if method is None:
-        temp = subject + " [SEP]" + object + " [SEP]" + sentence
+        temp = subject + " [SEP]" + object + " [SEP] " + sentence
 
     # entity mask
     elif method == "entity_mask":
