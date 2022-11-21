99d6ef47a75859ba732fab9a1bc8c349ee23d52e
wbbin0718 <wbbin0718@gmail.com>
2022-11-21 00:08:10 +0000

    feat: add extraction code

--- src/utils/representation.py
+++ src/utils/representation.py
@@ -1,3 +1,6 @@
+from utils.extraction import extraction
+
+
 def entity_representation(subject: str, object: str, sentence: str, method: str = None) -> str:
     """
     Args:
@@ -19,24 +22,9 @@ def entity_representation(subject: str, object: str, sentence: str, method: str 
         "typed_entity_marker_punct",
     ], "입력하신 method는 없습니다."
 
-    # subject object extraction
-
-    subject_entity = subject[:-1].split(",")[-1].split(":")[1]
-    object_entity = object[:-1].split(",")[-1].split(":")[1]
-
-    subject_length = len(subject.split(","))
-    object_length = len(object.split(","))
-
-    sub_start_idx = int(subject.split(",", subject_length - 3)[subject_length - 3].split(",")[0].split(":")[1])
-    sub_end_idx = int(subject.split(",", subject_length - 3)[subject_length - 3].split(",")[1].split(":")[1])
-    subject = "".join(subject.split(",", subject_length - 3)[: subject_length - 3]).split(":")[1]
-
-    obj_start_idx = int(object.split(",", object_length - 3)[object_length - 3].split(",")[0].split(":")[1])
-    obj_end_idx = int(object.split(",", object_length - 3)[object_length - 3].split(",")[1].split(":")[1])
-    object = "".join(object.split(",", object_length - 3)[: object_length - 3]).split(":")[1]
-
-    subject_entity = subject_entity.replace("'", "").strip()
-    object_entity = object_entity.replace("'", "").strip()
+    sub_start_idx, sub_end_idx, obj_start_idx, obj_end_idx, subject, object, subject_entity, object_entity = extraction(
+        subject, object
+    )
 
     # entity representation
 
