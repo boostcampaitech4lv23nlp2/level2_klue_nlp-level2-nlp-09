65a084db74d4987b66387260290d5e7c17e82129
FacerAin <ywsong.dev@kakao.com>
2022-11-29 01:07:53 +0000

    feat: update inference for custom model

--- src/inference.py
+++ src/inference.py
@@ -7,10 +7,12 @@ import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers import AutoConfig, AutoModelForSequenceClassification, AutoTokenizer
 
 from src.data_loader import REDataset, data_loader
-from src.utils import num_to_label
+from src.model import RBert, compute_metrics
+from src.utils import get_train_valid_split, label_to_num, num_to_label, save_model_remote, set_mlflow_logger, set_seed
+from src.utils.custom_trainer import CustomTrainer
 
 
 def predict(model, tokenized_sent, device):
@@ -27,9 +29,11 @@ def predict(model, tokenized_sent, device):
             outputs = model(
                 input_ids=data["input_ids"].to(device),
                 attention_mask=data["attention_mask"].to(device),
-                token_type_ids=data["token_type_ids"].to(device),
+                subject_mask=data["subject_mask"].to(device),
+                object_mask=data["object_mask"].to(device),
+                labels=None,
             )
-        logits = outputs[0]
+        logits = outputs["logits"]
         prob = F.softmax(logits, dim=-1).detach().cpu().numpy()
         logits = logits.detach().cpu().numpy()
         result = np.argmax(logits, axis=-1)
@@ -45,9 +49,31 @@ def inference(model_args, data_args, training_args):
     주어진 dataset csv 파일과 같은 형태일 경우 inference 가능한 코드입니다.
     """
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = AutoModelForSequenceClassification.from_pretrained(data_args.best_model_dir_path)
-    model.to(device)
+    model_config = AutoConfig.from_pretrained(model_args.model_name_or_path, num_labels=30)
+
     tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
+    special_tokens_dict = {"additional_special_tokens": ["<S>", "</S>", "<O>", "</O>"]}
+    tokenizer.add_special_tokens(special_tokens_dict)
+    """ model = RBert.from_pretrained(
+        data_args.best_model_dir_path,
+    )"""
+    model = RBert(model_config, tokenizer, model_args.model_name_or_path)
+    state_dict = torch.load("model.pt")
+    model.load_state_dict(state_dict)
+    model.to(device)
+
+    valid_raw_dataset = data_loader(data_args.validation_file_path)
+    valid_label = label_to_num(valid_raw_dataset["label"].values)
+    valid_dataset = REDataset(valid_raw_dataset, tokenizer, valid_label)
+    trainer = CustomTrainer(
+        model=model,  # the instantiated 🤗 Transformers model to be trained
+        args=training_args,  # training arguments, defined above  # training dataset
+        eval_dataset=valid_dataset,  # evaluation dataset
+        compute_metrics=compute_metrics,
+    )
+
+    metrics = trainer.evaluate(valid_dataset)
+    print(metrics)
 
     # new_tokens = pd.read_csv("src/new_tokens.csv").columns.tolist()
     # new_special_tokens = pd.read_csv("src/special_tokens.csv").columns.tolist()
